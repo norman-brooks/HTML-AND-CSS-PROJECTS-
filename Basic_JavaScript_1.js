26c47ad3c5cbf0c40b1778246c985a65366d7dda
norman-brooks <nbprogramming@outlook.com>
2024-10-20 14:57:21 -0700

    Update Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,6 +1,6 @@
 function My_First_Function () {
 	var String = "Kiss me, I'm Irish!";
 	var result = String.fontcolor("green");
-	document.getElementByID("Irish").innerHTML = 
+	document.getElementById("Irish").innerHTML = 
 	result;
 	}
